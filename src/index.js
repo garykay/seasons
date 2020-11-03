8f0396c34c1f3eaff9690c9055ab66e737a22186
Gary Readhead <roomworksmedia@gmail.com>
2020-11-03 13:35:02 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
 
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
+class App extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude })
+            },
+            (err) => console.log(err)
+        );
+
+    }
+
+    render() {
 
 
 
-    return (
-        <div className="container">
-            Latitude:
-        </div>
-    );
+        return (
+            <div className="container">
+                Latitude: {this.state.lat}
+            </div>
+        );
+    }
 }
 
 

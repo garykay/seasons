1e0c05f907d72bd33d1287bd9af2cb306b836329
Gary Readhead <roomworksmedia@gmail.com>
2020-11-03 11:17:22 +0000

    Getting ready for the App

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+const SeasonDisplay = () => {
+
+    return (
+        <div className="container">
+
+            Season Display
+        </div>
+    );
+
+}
+
+export default SeasonDisplay;

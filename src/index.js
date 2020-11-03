c1cd84d92e7dc4f28a0f09f3056a3d947c68ef3f
Gary Readhead <roomworksmedia@gmail.com>
2020-11-03 11:34:27 +0000

    Add geolocation to app - basic

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
+
+
+
     return (
         <div className="container">
-
-            Hi There
+            Latitude:
         </div>
     );
 }

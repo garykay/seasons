18bfcf42a0b93b3c913a9228c431fee833f26c3d
Gary Readhead <roomworksmedia@gmail.com>
2020-11-05 09:23:34 +0000

    Update SeasonDisplay.js

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+    summer: {
+        text: 'lets hit the beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Bur it is chilly',
+        iconName: 'snowflake'
+    }
+}
+
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
@@ -13,13 +24,18 @@ const SeasonDisplay = (props) => {
 
 
     const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[season]
 
-    console.log(season);
 
     return (
         <div className="container">
 
-            Season Display
+            <i className={`${iconName} massive icon`}></i>
+
+            <h1>{text}</h1>
+
+            <i className={`${iconName} massive icon`}></i>
+
         </div>
     );
 
